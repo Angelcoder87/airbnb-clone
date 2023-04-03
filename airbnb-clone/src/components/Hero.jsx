74b341f75bc021dd170ce6e39d3ddc98d555b943
Angela Odhiambo <angelaodhiambo87@gmail.com>
2023-04-03 15:20:33 +0300

    Creating the navbar and Hero using react

---
+++ airbnb-clone/src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+        <section className="hero">
+        <div class="photo-grid">
+    <div class="photo"><img src="../images/Photo5.jpg" /></div>
+    <div class="photo"><img src="../images/Photo2.jpg" /></div>
+    <div class="photo"><img src="../images/Photo3.jpg" /></div>
+    <div class="photo"><img src="../images/Photo4.jpg" /></div>
+    <div class="photo"><img src="../images/Photo1.jpg" /></div>
+    <div class="photo"><img src="../images/Photo6.jpg" /></div>
+        </div>
+        <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}
